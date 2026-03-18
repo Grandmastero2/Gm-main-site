9f5038d55cb1724e1e5306c11b4d150626c59cf4
Grandmastero2 <grandmaster192837@gmail.com>
2026-03-18 15:07:56 +0200

    site1
    v1.1

---
+++ GrandMaster/main.js
@@ -0,0 +1,17 @@
+function HGS(){
+    window.open("https://hawk-games-studio.itch.io","_blank")
+}
+
+
+
+
+function Youtube(){
+
+}
+
+
+
+
+function MD(){
+    window.open("https://hawk-games-studio.itch.io/multi-dudes","_blank")
+}
