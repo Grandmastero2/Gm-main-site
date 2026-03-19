fad79cf7c8401508310c10a411594f6c648252a7
Mohamed Rajab <mr8187649@gmail.com>
2026-03-19 05:22:57 +0200

    Done

---
+++ main.js
@@ -0,0 +1,17 @@
+function HGS(){
+    window.open("https://hawk-games-studio.itch.io","_blank")
+}
+
+
+
+
+function Youtube(){
+    window.open("https://www.youtube.com/@grandmaster_3048/featured","_blank")
+}
+
+
+
+
+function MD(){
+    window.open("https://hawk-games-studio.itch.io/multi-dudes","_blank")
+}
